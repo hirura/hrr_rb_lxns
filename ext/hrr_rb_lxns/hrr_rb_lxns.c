6193e64ff1047f5ccc4508a200c9b52d1aa6d480
hirura <hirura@gmail.com>
2020-03-13 19:53:16 +0900

    implement primitive unshare operation

--- ext/hrr_rb_lxns/hrr_rb_lxns.c
+++ ext/hrr_rb_lxns/hrr_rb_lxns.c
@@ -1,9 +1,81 @@
 #include "hrr_rb_lxns.h"
+#define _GNU_SOURCE 1
+#include <sched.h>
 
 VALUE rb_mHrrRbLxns;
+VALUE rb_mHrrRbLxnsConst;
+
+/*
+ * A primitive wrapper around unshare(2) system call.
+ * Disassociates parts of the caller process's execution context.
+ *
+ * == Synopsis:
+ *   # Disassociates uts namespace
+ *   File.readlink "/proc/self/ns/uts"       # => uts:[aaa]
+ *   HrrRbLxns.__unshare__ HrrRbLxns::NEWUTS # => 0
+ *   File.readlink "/proc/self/ns/uts"       # => uts:[xxx]
+ *
+ *   # Disassociates uts and mount namespaces
+ *   File.readlink "/proc/self/ns/uts"                          # => uts:[aaa]
+ *   File.readlink "/proc/self/ns/mnt"                          # => mnt:[bbb]
+ *   HrrRbLxns.__unshare__ HrrRbLxns::NEWUTS | HrrRbLxns::NEWNS # => 0
+ *   File.readlink "/proc/self/ns/uts"                          # => uts:[xxx]
+ *   File.readlink "/proc/self/ns/mnt"                          # => mnt:[yyy]
+ *
+ * @param flags [Integer] Represents the namespaces to disassociate.
+ * @return [Integer] 0.
+ * @raise [TypeError] In case the given flags cannot be converted to integer.
+ * @raise [Errno::EXXX]  In case unshare(2) system call failed.
+ */
+VALUE
+hrr_rb_lxns_unshare(VALUE self, VALUE flags)
+{
+  if (unshare(NUM2INT(flags)) < 0)
+    rb_sys_fail("unshare");
+
+  return INT2FIX(0);
+}
 
 void
 Init_hrr_rb_lxns(void)
 {
   rb_mHrrRbLxns = rb_define_module("HrrRbLxns");
+
+  rb_define_singleton_method(rb_mHrrRbLxns, "__unshare__", hrr_rb_lxns_unshare, 1);
+
+  rb_mHrrRbLxnsConst = rb_define_module_under(rb_mHrrRbLxns, "Constants");
+  rb_include_module(rb_mHrrRbLxns, rb_mHrrRbLxnsConst);
+
+#ifdef CLONE_NEWIPC
+  /* Represents ipc namespace. */
+  rb_define_const(rb_mHrrRbLxnsConst, "NEWIPC", INT2FIX(CLONE_NEWIPC));
+#endif
+#ifdef CLONE_NEWNS
+  /* Represents mount namespace. */
+  rb_define_const(rb_mHrrRbLxnsConst, "NEWNS", INT2FIX(CLONE_NEWNS));
+#endif
+#ifdef CLONE_NEWNET
+  /* Represents network namespace. */
+  rb_define_const(rb_mHrrRbLxnsConst, "NEWNET", INT2FIX(CLONE_NEWNET));
+#endif
+#ifdef CLONE_NEWPID
+  /* Represents pid namespace. */
+  rb_define_const(rb_mHrrRbLxnsConst, "NEWPID", INT2FIX(CLONE_NEWPID));
+#endif
+#ifdef CLONE_NEWUTS
+  /* Represents uts namespace. */
+  rb_define_const(rb_mHrrRbLxnsConst, "NEWUTS", INT2FIX(CLONE_NEWUTS));
+#endif
+#ifdef CLONE_NEWUSER
+  /* Represents user namespace. */
+  rb_define_const(rb_mHrrRbLxnsConst, "NEWUSER", INT2FIX(CLONE_NEWUSER));
+#endif
+#ifdef CLONE_NEWCGROUP
+  /* Represents cgroup namespace. */
+  rb_define_const(rb_mHrrRbLxnsConst, "NEWCGROUP", INT2FIX(CLONE_NEWCGROUP));
+#endif
+#ifdef CLONE_NEWTIME
+  /* Represents time namespace. */
+  rb_define_const(rb_mHrrRbLxnsConst, "NEWTIME", INT2FIX(CLONE_NEWTIME));
+#endif
 }
--- lib/hrr_rb_lxns.rb
+++ lib/hrr_rb_lxns.rb
@@ -1,7 +1,67 @@
 require "hrr_rb_lxns/version"
 require "hrr_rb_lxns/hrr_rb_lxns"
 
+# Utilities working with Linux namespaces for CRuby.
 module HrrRbLxns
-  class Error < StandardError; end
-  # Your code goes here...
+
+  # Constants that represent the flags for Linux namespaces operations.
+  module Constants
+  end
+
+  # A wrapper around unshare(2) system call.
+  #
+  # == Synopsis:
+  #   # Disassociates uts namespace
+  #   File.readlink "/proc/self/ns/uts"   # => uts:[aaa]
+  #   HrrRbLxns.unshare HrrRbLxns::NEWUTS # => 0
+  #   File.readlink "/proc/self/ns/uts"   # => uts:[xxx]
+  #
+  #   # Disassociates uts and mount namespaces
+  #   File.readlink "/proc/self/ns/uts"   # => uts:[aaa]
+  #   File.readlink "/proc/self/ns/mnt"   # => mnt:[bbb]
+  #   HrrRbLxns.unshare "um"              # => 0
+  #   File.readlink "/proc/self/ns/uts"   # => uts:[xxx]
+  #   File.readlink "/proc/self/ns/mnt"   # => mnt:[yyy]
+  #
+  # @param flags [Integer] An integer value that represents namespaces to disassociate.
+  # @param flags [String] A string that represents namespaces. The mapping of charactors and flags are: <br>
+  #   "i" : NEWIPC <br>
+  #   "m" : NEWNS <br>
+  #   "n" : NEWNET <br>
+  #   "p" : NEWPID <br>
+  #   "u" : NEWUTS <br>
+  #   "U" : NEWUSER <br>
+  #   "C" : NEWCGROUP <br>
+  #   "T" : NEWTIME <br>
+  # @return [Integer] 0.
+  # @raise [ArgumentError] When given flags argument is not appropriate.
+  def self.unshare flags
+    _flags = interpret_flags flags
+    __unshare__ _flags
+  end
+
+  private
+
+  def self.interpret_flags arg
+    case arg
+    when Integer then arg
+    when String  then chars_to_flags arg
+    else raise TypeError, "unsupported flags: #{arg.inspect}"
+    end
+  end
+
+  def self.chars_to_flags chars
+    chars.each_char.inject(0) do |f, c|
+      if    c == "i" && const_defined?(:NEWIPC)    then f | NEWIPC
+      elsif c == "m" && const_defined?(:NEWNS)     then f | NEWNS
+      elsif c == "n" && const_defined?(:NEWNET)    then f | NEWNET
+      elsif c == "p" && const_defined?(:NEWPID)    then f | NEWPID
+      elsif c == "u" && const_defined?(:NEWUTS)    then f | NEWUTS
+      elsif c == "U" && const_defined?(:NEWUSER)   then f | NEWUSER
+      elsif c == "C" && const_defined?(:NEWCGROUP) then f | NEWCGROUP
+      elsif c == "T" && const_defined?(:NEWTIME)   then f | NEWTIME
+      else raise ArgumentError, "unsupported flag charactor: #{c.inspect}"
+      end
+    end
+  end
 end
