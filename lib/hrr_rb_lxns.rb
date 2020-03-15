a24f62c73cc10490aae8e275ebd2d1491b1df8c4
hirura <hirura@gmail.com>
2020-03-15 23:07:15 +0900

    implement setns wrapper

--- lib/hrr_rb_lxns.rb
+++ lib/hrr_rb_lxns.rb
@@ -35,9 +35,78 @@ module HrrRbLxns
   #   "T" : NEWTIME <br>
   # @return [Integer] 0.
   # @raise [ArgumentError] When given flags argument is not appropriate.
+  # @raise [Errno::EXXX] In case unshare(2) system call failed.
+
   def self.unshare flags
     _flags = interpret_flags flags
     __unshare__ _flags
+  end
+
+  # A wrapper around setns(2) system call.
+  #
+  # == Synopsis:
+  #   pid = fork do
+  #     # Disassociates uts namespace
+  #     File.readlink "/proc/self/ns/uts"    # => uts:[xxx]
+  #     HrrRbLxns.unshare HrrRbLxns::NEWUTS  # => 0
+  #     File.readlink "/proc/self/ns/uts"    # => uts:[yyy]
+  #     sleep
+  #   end
+  #   # Aassociates uts namespace
+  #   File.readlink "/proc/self/ns/uts"      # => uts:[xxx]
+  #   HrrRbLxns.setns HrrRbLxns::NEWUTS, pid # => 0
+  #   File.readlink "/proc/self/ns/uts"      # => uts:[yyy]
+  #
+  # @param flags [Integer] An integer value that represents namespaces to disassociate.
+  # @param flags [String] A string that represents namespaces. The mapping of charactors and flags are: <br>
+  #   "i" : NEWIPC <br>
+  #   "m" : NEWNS <br>
+  #   "n" : NEWNET <br>
+  #   "p" : NEWPID <br>
+  #   "u" : NEWUTS <br>
+  #   "U" : NEWUSER <br>
+  #   "C" : NEWCGROUP <br>
+  #   "T" : NEWTIME <br>
+  # @return [Integer] 0.
+  # @raise [ArgumentError] When given flags argument is not appropriate.
+  # @raise [Errno::EXXX] In case setns(2) system call failed.
+  def self.setns flags, pid
+    _flags = interpret_flags flags
+    fds = get_fds _flags, pid
+    fds.each do |path, nstype|
+      begin
+        fd = File.open(path, File::RDONLY)
+        __setns__ fd.fileno, nstype
+      ensure
+        fd.close rescue nil
+      end
+    end
+  end
+
+  def self.get_fds flags, pid
+    list = Array.new
+    list.push ["ipc",    NEWIPC   ] if const_defined?(:NEWIPC)
+    list.push ["mnt",    NEWNS    ] if const_defined?(:NEWNS)
+    list.push ["net",    NEWNET   ] if const_defined?(:NEWNET)
+    list.push ["pid",    NEWPID   ] if const_defined?(:NEWPID)
+    list.push ["uts",    NEWUTS   ] if const_defined?(:NEWUTS)
+    list.push ["user",   NEWUSER  ] if const_defined?(:NEWUSER)
+    list.push ["cgroup", NEWCGROUP] if const_defined?(:NEWCGROUP)
+    list.push ["time",   NEWTIME  ] if const_defined?(:NEWTIME)
+    fds = Array.new
+    list.each do |name, flag|
+      fd = get_fd name, (flags & flag), pid
+      fds.push [fd, flag] if fd
+    end
+    fds
+  end
+
+  def self.get_fd name, flag, pid
+    if flag.zero?.! && pid
+      "/proc/#{pid}/ns/#{name}"
+    else
+      nil
+    end
   end
 
   private
