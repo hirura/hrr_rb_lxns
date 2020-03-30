b6dfee5ed8b3a989be6c485fb7d7d9d5a0ccdbc9
hirura <hirura@gmail.com>
2020-03-30 10:53:26 +0000

    setns to support specifying file to associate with

--- lib/hrr_rb_lxns.rb
+++ lib/hrr_rb_lxns.rb
@@ -68,19 +68,36 @@ module HrrRbLxns
   #   "U" : NEWUSER <br>
   #   "C" : NEWCGROUP <br>
   #   "T" : NEWTIME <br>
-  # @param options [Hash] For future use.
+  # @param pid [Integer] Specifies a target process('s namespace) which the caller is to associate with. The paths specifying namespaces specified by pid are: <br>
+  #   /proc/pid/ns/mnt :    mount namespace <br>
+  #   /proc/pid/ns/uts :    uts namespace <br>
+  #   /proc/pid/ns/ipc :    ipc namespace <br>
+  #   /proc/pid/ns/net :    network namespace <br>
+  #   /proc/pid/ns/pid :    pid namespace <br>
+  #   /proc/pid/ns/user :   user namespace <br>
+  #   /proc/pid/ns/cgroup : cgroup namespace <br>
+  #   /proc/pid/ns/time :   time namespace <br>
+  # @param options [Hash]
+  # @option options [String] :mount A file which specifies the mount namespace to associate with.
+  # @option options [String] :uts A file which specifies the uts namespace to associate with.
+  # @option options [String] :ipc A file which specifies the ipc namespace to associate with.
+  # @option options [String] :network A file which specifies the network namespace to associate with.
+  # @option options [String] :pid A file which specifies the pid namespace to associate with.
+  # @option options [String] :user A file which specifies the user namespace to associate with.
+  # @option options [String] :cgroup A file which specifies the cgroup namespace to associate with.
+  # @option options [String] :time A file which specifies the time namespace to associate with.
   # @return [Integer] 0.
-  # @raise [ArgumentError] When given flags argument is not appropriate.
+  # @raise [ArgumentError] When given flags argument is not appropriate or when given pid and/or options are not appropriate for the given flags.
   # @raise [Errno::EXXX] In case setns(2) system call failed.
   def self.setns flags, pid, options={}
     _flags = interpret_flags flags
-    fds = get_fds _flags, pid
-    fds.each do |path, nstype|
+    files = get_files _flags, pid, options
+    files.each do |path, nstype|
       begin
-        fd = File.open(path, File::RDONLY)
-        __setns__ fd.fileno, nstype
+        file = File.open(path, File::RDONLY)
+        __setns__ file.fileno, nstype
       ensure
-        fd.close rescue nil
+        file.close rescue nil
       end
     end
   end
@@ -110,27 +127,33 @@ module HrrRbLxns
     end
   end
 
-  def self.get_fds flags, pid
+  def self.get_files flags, pid, options
     list = Array.new
-    list.push ["ipc",    NEWIPC   ] if const_defined?(:NEWIPC)
-    list.push ["mnt",    NEWNS    ] if const_defined?(:NEWNS)
-    list.push ["net",    NEWNET   ] if const_defined?(:NEWNET)
-    list.push ["pid",    NEWPID   ] if const_defined?(:NEWPID)
-    list.push ["uts",    NEWUTS   ] if const_defined?(:NEWUTS)
-    list.push ["user",   NEWUSER  ] if const_defined?(:NEWUSER)
-    list.push ["cgroup", NEWCGROUP] if const_defined?(:NEWCGROUP)
-    list.push ["time",   NEWTIME  ] if const_defined?(:NEWTIME)
-    fds = Array.new
-    list.each do |name, flag|
-      fd = get_fd name, (flags & flag), pid
-      fds.push [fd, flag] if fd
+    list.push ["ipc",    NEWIPC,    :ipc    ] if const_defined?(:NEWIPC)
+    list.push ["mnt",    NEWNS,     :mount  ] if const_defined?(:NEWNS)
+    list.push ["net",    NEWNET,    :network] if const_defined?(:NEWNET)
+    list.push ["pid",    NEWPID,    :pid    ] if const_defined?(:NEWPID)
+    list.push ["uts",    NEWUTS,    :uts    ] if const_defined?(:NEWUTS)
+    list.push ["user",   NEWUSER,   :user   ] if const_defined?(:NEWUSER)
+    list.push ["cgroup", NEWCGROUP, :cgroup ] if const_defined?(:NEWCGROUP)
+    list.push ["time",   NEWTIME,   :time   ] if const_defined?(:NEWTIME)
+    files = Array.new
+    list.each do |name, flag, key|
+      file = get_file name, (flags & flag), pid, key, options[key]
+      files.push [file, flag] if file
     end
-    fds
+    files
   end
 
-  def self.get_fd name, flag, pid
-    if flag.zero?.! && pid
-      "/proc/#{pid}/ns/#{name}"
+  def self.get_file name, flag, pid, key, option
+    if flag.zero?.!
+      if option
+        option
+      elsif pid
+        "/proc/#{pid}/ns/#{name}"
+      else
+        raise ArgumentError, "neither pid nor options[:#{key}] specified for #{key} namespace"
+      end
     else
       nil
     end
